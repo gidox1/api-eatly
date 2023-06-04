dc2d8a9ee580744af3aefece12e2ea06283fb174
gidox1 <gideonodiase1@gmail.com>
2023-06-04 22:46:17 +0100

    Finish webhook: Customer created

--- app/lib/mq/consumers/createCustomer.js
+++ app/lib/mq/consumers/createCustomer.js
@@ -4,9 +4,6 @@ import amqp from 'amqplib/callback_api.js';
 import { consumer } from '../consumer.js';
 import * as dotenv from 'dotenv';
 import client from '../../square.js';
-// import UserService from '../../../modules/user/user.service.js';
-// import { getDb } from '../../../db.js';
-// import { isErr, toJSON } from "true-myth/result";
 
 dotenv.config();
 
@@ -25,20 +22,8 @@ class HandleCustomerCreated {
         resp = JSON.parse(resp);
 
         const payload = JSON.parse(resp.message);
-        // const userId = payload.userId;
         delete payload.userId;
         await client.customersApi.createCustomer(payload);
-        // const userRepository = await getDb(config.mongo.collections.user.name);
-        // const customerId = call.result.customer.id;
-        // metrics.customerId = customerId;
-
-        // // const updateUserResponse = await new UserService(logger, config, userRepository, {}, {}).update({
-        // //   paymentProviderCustomerId: customerId,
-        // // }, userId);
-
-        // // if (isErr(updateUserResponse)) {
-        // //   throw new Error('failed to update user')
-        // // }
         metrics.isSuccess = true; 
       });
     } catch(E) {
--- app/webhook.js
+++ app/webhook.js
@@ -4,27 +4,20 @@ import { getDb } from "./db.js";
 import { Collection } from "mongodb";
 import * as dotenv from 'dotenv';
 import { WebhooksHelper } from "square";
+import UserService from "./modules/user/user.service.js";
+import logger from "./lib/logger.js";
+import { isErr } from "true-myth/result";
 
 dotenv.config();
-
-  // /**
-  //  * @param {Config} config
-  //  * @returns {Promise<Collection<Document>}
-  //  */
-  // const getWebhookEventsRepository = async (config) => {
-  //   return getDb(config.mongo.collections.webhookEvents.name);
-  // }
-
 
 // The URL where event notifications are sent.
 const NOTIFICATION_URL = 'https://eatly-api.onrender.com/webhook';
 
 // The signature key defined for the subscription.
-const SIGNATURE_KEY = '3gDlnzICzVO7cQ1RT9gGaw';
+const SIGNATURE_KEY = process.env.WEBHOOK_SECRET;
 
 // isFromSquare generates a signature from the url and body and compares it to the Square signature header.
 function isFromSquare(signature, body) {
-  console.log("VALIDATING");
   return WebhooksHelper.isValidWebhookEventSignature(
       body,
       signature,
@@ -33,40 +26,13 @@ function isFromSquare(signature, body) {
     );
 }
 
-function requestHandler(request, response) {
-  let body = '';
-  request.setEncoding('utf8');
-
-  request.on('data', function(chunk) {
-    body += chunk;
-  });
-
-  request.on('end', function() {
-    const signature = request.headers['x-square-hmacsha256-signature'];
-    if (isFromSquare(signature, body)) {
-      // Signature is valid. Return 200 OK.
-      response.writeHead(200);
-      console.info("Request body: " + body);
-    } else {
-      console.log("INVALID SIGNATURE")
-      // Signature is invalid. Return 403 Forbidden.
-      response.writeHead(200);
-    }
-    response.end();
-  });
+/**
+ * @param {Config} config
+ * @returns {Promise<Collection<Document>}
+ */
+const getWebhookEventsRepository = async (config) => {
+  return getDb(config.mongo.collections.webhookEvents.name);
 }
-
-// // Start a simple server for local testing.
-// // Different frameworks may provide the raw request body in other ways.
-// // INSTRUCTIONS
-// // 1. Run the server:
-// //    node server.js
-// // 2. Send the following request from a separate terminal:
-// //    curl -vX POST localhost:8000 -d '{"hello":"world"}' -H "X-Square-HmacSha256-Signature: 2kRE5qRU2tR+tBGlDwMEw2avJ7QM4ikPYD/PJ3bd9Og="
-// const server = http.createServer(requestHandler);
-// server.listen(8000);
-
-
 
 /**
  * @param {Request} req 
@@ -74,61 +40,71 @@ function requestHandler(request, response) {
  * @param {import("express").NextFunction} next 
  * @param {Config} config
  */
-export const webhook = async (req, res, config) => {
-  requestHandler(req, res);
-  // const event = req.body;
-  // const metrics = {};
-  // metrics.eventType = event.type;
-  // metrics.eventId = event.event_id;
+export const webhook = async (request, response, config) => {
+  let body = '';
+  request.setEncoding('utf8');
 
-  // const webhookSecret = config.square.webHookSecret;
+  request.on('data', function(chunk) {
+    body += chunk;
+  });
 
-  // // dedupe
-  // const repository = await getWebhookEventsRepository(config);
-  // const eventData = await repository.findOne({
-  //   id: event.event_id,
-  // });
-
-  // // if(eventData !== null) {
-  // //   console.log('Duplicate event detected:', eventId);
-  // //   return res.status(200).end();
-  // // }
-
-
-  // const signature = req.headers['x-square-hmacsha256-signature'];
-  // const url = '	https://eatly-api.onrender.com/webhook';
-
-  // const isValidSignature = verifySignature(url, req.body.toString(), signature, webhookSecret);
-  // if (!isValidSignature) {
-  //   console.error('\n\nInvalid webhook signature\n\n');
-  //   return res.status(200).end();
-  // }
-  
-  // console.log("All good!")
-  // res.status(200).end();
-
-
-  // // Verify the webhook event signature
-  // // const signature = req.headers['x-square-signature'];
-  // // const isValidSignature = verifySignature(JSON.stringify(req.body), signature, webhookSecret);
-  // // if (!isValidSignature) {
-  // //   console.error('Invalid webhook signature');
-  // //   return res.status(400).end();
-  // // }
-
-  // // Process the webhook event
-  // // Add your custom logic here to handle the event
-
-  // res.status(200).end();
+  request.on('end', async function() {
+    const signature = request.headers['x-square-hmacsha256-signature'];
+    if (isFromSquare(signature, body)) {
+      await HandleEvents(body, config);
+      response.writeHead(200);
+    } else {
+      console.log("INVALID SIGNATURE")
+      // Signature is invalid. Return 403 Forbidden.
+      response.writeHead(403);
+    }
+    response.end();
+  });
 }
 
-// Helper function to verify the webhook signature
-// function verifySignature(requestUrl, payload, signature, secret) {
-//   console.log(requestUrl, payload, signature, secret, "requestUrl, payload, signature, secret")
-//   return WebhooksHelper.isValidWebhookEventSignature(
-//     payload,
-//     signature,
-//     secret,
-//     requestUrl
-//   );
-// }
+
+const HandleEvents = async (event, config) => {
+  event = JSON.parse(event);
+  // dedupe
+  const webhookEventRepository = await getWebhookEventsRepository(config);
+  const eventData = await webhookEventRepository.findOne({
+    id: event.event_id,
+  });
+
+  if(eventData !== null) {
+    logger.error('Duplicate event detected:', {
+      eventId: event.event_id,
+    });
+    return;
+  }
+
+  switch(event.type) {
+    case 'customer.created':
+      const metrics = {};
+      metrics.eventType = 'customer.created';
+      const userRepository = await getDb(config.mongo.collections.user.name);
+      const user = await userRepository.findOne({ email: event.data.object.customer.email_address });
+      const customerId = event.data.id;
+      metrics.customerId = customerId;
+      metrics.userId = user._id.toHexString();
+
+      const updateUserResponse = await new UserService(logger, config, userRepository, {}, {}).update({
+        paymentProviderCustomerId: customerId,
+      }, user._id.toHexString());
+
+      if (isErr(updateUserResponse)) {
+        throw new Error('failed to update user')
+      }
+
+      await webhookEventRepository.insertOne({
+        type: event.type,
+        id: event.event_id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+      logger.log('successfully handled customer created event', metrics);
+      break;
+    default:
+      console.log('default event!');
+  }
+}
